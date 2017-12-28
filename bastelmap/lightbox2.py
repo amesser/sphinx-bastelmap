5fe9dc4ad5e7ec6211a6b35030159775c90f656f
Andreas Messer <andi@bastelmap.de>
2017-12-28 17:26:30 +0100

    Fix missing import

--- bastelmap/lightbox2.py
+++ bastelmap/lightbox2.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from docutils import nodes
+from docutils.parsers.rst import directives
 from docutils.parsers.rst.directives import images
 from sphinx.transforms import SphinxTransform
 from sphinx.util.osutil import ensuredir, copyfile
